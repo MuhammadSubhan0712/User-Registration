70045438715e19c6bce4ffc133fc9b3a663f4337
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-07-27 16:16:33 +0500

    Complete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import {
   getAuth,
-  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { auth } from "./config.js";
 
