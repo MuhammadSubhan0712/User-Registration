a1ffbc28e35eba315d9077c104d1dc411cfcf200
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-07-27 16:42:31 +0500

    Complete

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { auth } from "./config.js";
 
+
 const form = document.querySelector("#form");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
@@ -11,7 +12,6 @@ const display = document.querySelector("#div");
 
 form.addEventListener("submit", (events) => {
   events.preventDefault();
-  const auth = getAuth();
 
   signInWithEmailAndPassword(auth, email.value, password.value)
 
