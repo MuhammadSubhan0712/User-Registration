4023db91e2d1d416a68aae73c0c34be5c3e24db0
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-07-27 16:47:06 +0500

    Complete

--- home.js
+++ home.js
@@ -1,15 +1,15 @@
 
-
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const auth = getAuth();
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
     // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+  } else {
+    // User is signed out
+    // ...
+  }
+});
