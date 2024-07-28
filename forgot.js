8636fb2a3d15cf3d9b8c27dd3156131a4fe969ee
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-07-28 15:28:39 +0500

    Complete

---
+++ forgot.js
@@ -0,0 +1,10 @@
+
+
+
+
+const form = document.querySelector('forgot-pasword');
+
+const email = document.querySelector('forgot-pasword');
+const forgot_btn = document.querySelector('forgot-btn');
+
+
