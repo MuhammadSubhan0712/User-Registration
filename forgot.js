755d028088425f7eeae7daa0b958ea0bebfd7d36
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-07-28 15:30:31 +0500

    Complete

--- forgot.js
+++ forgot.js
@@ -1,10 +1,15 @@
-
-
-
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+  import { auth } from "./config.js";
+  
 
 const form = document.querySelector('forgot-pasword');
 
-const email = document.querySelector('forgot-pasword');
+const email = document.querySelector('email');
 const forgot_btn = document.querySelector('forgot-btn');
+
+
 
 
